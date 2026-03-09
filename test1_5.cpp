9e86cc5b6fe3adc85a8502672e1e81fe7177c574
desiredeternity <don.bdf2011@mail.ru>
2026-03-09 22:19:13 +0300

    Revert "remove test from repo (will be returned with right encoding)"
    This reverts commit 9b9b2210244c78c5f9bd10c75733818a2f42316c.

---
+++ test1_5.cpp
@@ -0,0 +1,16 @@
+#include "iotest.hpp"
+
+int main() {
+    test_exact("5 10 1582",    "15 10 1582");
+    test_exact("1 3 1900",     "14 3 1900");
+    test_exact("29 2 2000",    "13 3 2000");
+    test_exact("1 1 2024",     "14 1 2024");
+    test_exact("30 12 2023",   "12 1 2024");
+    test_exact("1 3 2100",     "15 3 2100");
+    test_exact("1 3 100",      "28 2 100");
+    test_exact("1 1 1",        "-1 1 1");
+    test_contains("1 13 1600", "������");
+    test_contains("40 2 1500", "������");
+
+    return 0;
+}
