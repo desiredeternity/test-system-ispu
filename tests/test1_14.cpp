14b44a83b2500602e0979d60466eace079af0e75
desiredeternity <don.bdf2011@mail.ru>
2026-03-26 23:18:27 +0300

    add test 1_14

---
+++ tests/test1_14.cpp
@@ -0,0 +1,10 @@
+#include "iotest.hpp"
+
+int main() {
+    test_exact("4 4 6 5",      "Один ход");
+    test_any_match("4 4 5 5",  {"6 3", "3 6"});
+    test_exact("1 1 1 2",      "Нельзя попасть");
+    test_exact("1 1 2 3",      "Один ход");
+    test_any_match("5 3 2 6",  {"3 4", "4 5"});
+    test_contains("1 9 2 4",   "Ошибка");
+}
