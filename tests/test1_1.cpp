190e67682816800de4ab60d53e8bcbe4d8482fad
desiredeternity <don.bdf2011@mail.ru>
2026-03-26 23:18:46 +0300

    refactor test 1_1

--- tests/test1_1.cpp
+++ tests/test1_1.cpp
@@ -1,102 +1,13 @@
 #include "iotest.hpp"
-#include <iostream>
-
-using namespace std;
-
-void test1(string input, string expected) {
-    string result = run_with_input(input);
-
-    if (result.find("зел") != string::npos && result.find("крыс") != string::npos) {
-        cout << "Тест: " << "ПРОЙДЕН\n";
-    }
-    else {
-        cout << "Тест: " << "ОШИБКА: ";
-        cout << "ожидалось \'" << expected << "\', ";
-        cout << "получено \'" << result << "\', ";
-        cout << "введено \'" << input << "\'\n";
-    }
-}
-
-void test2(string input, string expected) {
-    string result = run_with_input(input);
-
-    if (result.find("бел") != string::npos && result.find("зме") != string::npos) {
-        cout << "Тест: " << "ПРОЙДЕН\n";
-    }
-    else {
-        cout << "Тест: " << "ОШИБКА: ";
-        cout << "ожидалось \'" << expected << "\', ";
-        cout << "получено \'" << result << "\', ";
-        cout << "введено \'" << input << "\'\n";
-    }
-}
-
-void test3(string input, string expected) {
-    string result = run_with_input(input);
-
-    if ((result.find("жел") != string::npos || result.find("жёл") != string::npos)
-      && result.find("крыс") != string::npos) {
-        cout << "Тест: " << "ПРОЙДЕН\n";
-    }
-    else {
-        cout << "Тест: " << "ОШИБКА: ";
-        cout << "ожидалось \'" << expected << "\', ";
-        cout << "получено \'" << result << "\', ";
-        cout << "введено \'" << input << "\'\n";
-    }
-}
-
-void test4(string input, string expected) {
-    string result = run_with_input(input);
-
-    if (result.find("зел") != string::npos && result.find("свин") != string::npos) {
-        cout << "Тест: " << "ПРОЙДЕН\n";
-    }
-    else {
-        cout << "Тест: " << "ОШИБКА: ";
-        cout << "ожидалось \'" << expected << "\', ";
-        cout << "получено \'" << result << "\', ";
-        cout << "введено \'" << input << "\'\n";
-    }
-}
-
-void test5(string input, string expected) {
-    string result = run_with_input(input);
-
-    if ((result.find("чер") != string::npos || result.find("чёр") != string::npos)
-      && result.find("обез") != string::npos) {
-        cout << "Тест: " << "ПРОЙДЕН\n";
-    }
-    else {
-        cout << "Тест: " << "ОШИБКА: ";
-        cout << "ожидалось \'" << expected << "\', ";
-        cout << "получено \'" << result << "\', ";
-        cout << "введено \'" << input << "\'\n";
-    }
-}
-
-void test6(string input, string expected) {
-    string result = run_with_input(input);
-
-    if (result.find("зел") != string::npos && result.find("крыс") != string::npos) {
-        cout << "Тест: " << "ПРОЙДЕН\n";
-    }
-    else {
-        cout << "Тест: " << "ОШИБКА: ";
-        cout << "ожидалось \'" << expected << "\', ";
-        cout << "получено \'" << result << "\', ";
-        cout << "введено \'" << input << "\'\n";
-    }
-}
 
 int main() {
-    test1("1984",         "Год зеленой крысы");
-    test2("1785",         "Год белой змеи");
-    test3("1996",         "Год желтой крысы");
-    test4("1995",         "Год зеленой свиньи");
-    test5("0",            "Год черной обезьяны");
-    test6("2044",         "Год зеленой крысы");
-    test_contains("-1",   "Ошибка");
+    test_any_match("1984", {"Год зеленой крысы", "Год зелёной крысы", "Год зеленого крысы", "Год зелёного крысы"});
+    test_any_match("1785", {"Год белой змеи", "Год белого змеи"});
+    test_any_match("1996", {"Год желтой крысы", "Год жёлтой крысы", "Год желтого крысы", "Год жёлтого крысы"});
+    test_any_match("1995", {"Год зеленой свиньи", "Год зелёной свиньи", "Год зеленого свиньи", "Год зелёного свиньи"});
+    test_any_match("0",    {"Год черной обезьяны", "Год чёрной обезьяны", "Год черного обезьяны", "Год чёрного обезьяны"});
+    test_any_match("2044", {"Год зеленой крысы", "Год зелёной крысы", "Год зеленого крысы", "Год зелёного крысы"});
+    test_contains("-1",    "Ошибка");
 
     return 0;
 }
