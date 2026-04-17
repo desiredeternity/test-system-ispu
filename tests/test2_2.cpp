897ebeaf49ae6ae199936ee368ab18d4361e41f1
desiredeternity <don.bdf2011@mail.ru>
2026-04-17 23:14:40 +0300

    add test 2_2

---
+++ tests/test2_2.cpp
@@ -0,0 +1,12 @@
+#include "iotest.hpp"
+
+int main() {
+    test_num_array("1 10",           "2 3 5 7");
+    test_exact("8 10",               "В промежутке нет простых чисел");
+    test_num_array("50 100",         "53 59 61 67 71 73 79 83 89 97");
+    test_num_array("2 2",            "2");
+    test_num_array("999980 1000000", "999983");
+    test_exact("6 6",                "В промежутке нет простых чисел");
+    test_num_array("-10 10",         "2 3 5 7");
+    test_contains("10 1",            {"Ошибка", "2 3 5 7"});
+}
